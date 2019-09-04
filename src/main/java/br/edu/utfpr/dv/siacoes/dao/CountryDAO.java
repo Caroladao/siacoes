5a12a37ae1ac4aba18b0361c09a6eee52689d54e
Carol Adão <Carolineadao1@gmail.com>
2019-09-03 22:31:29 -0300

    correcting factory

--- src/main/java/br/edu/utfpr/dv/siacoes/dao/CountryDAO.java
+++ src/main/java/br/edu/utfpr/dv/siacoes/dao/CountryDAO.java
@@ -12,90 +12,90 @@ import br.edu.utfpr.dv.siacoes.log.UpdateEvent;
 import br.edu.utfpr.dv.siacoes.model.Country;
 
 public class CountryDAO implements IDaoFactory<Country> {
-	
+
 	public List<Country> listAll() throws SQLException {
 		Connection conn = null;
 		Statement stmt = null;
 		ResultSet rs = null;
-		
-		try{
+
+		try {
 			conn = ConnectionDAO.getInstance().getConnection();
 			stmt = conn.createStatement();
-		
+
 			rs = stmt.executeQuery("SELECT * FROM country ORDER BY name");
-			
+
 			List<Country> list = new ArrayList<Country>();
-			
-			while(rs.next()){
+
+			while (rs.next()) {
 				list.add(this.loadObject(rs));
 			}
-			
+
 			return list;
-		}finally{
-			if((rs != null) && !rs.isClosed())
+		} finally {
+			if ((rs != null) && !rs.isClosed())
 				rs.close();
-			if((stmt != null) && !stmt.isClosed())
+			if ((stmt != null) && !stmt.isClosed())
 				stmt.close();
-			if((conn != null) && !conn.isClosed())
+			if ((conn != null) && !conn.isClosed())
 				conn.close();
 		}
 	}
-	
-	public Country findById(int id) throws SQLException{
+
+	public Country findById(int id) throws SQLException {
 		Connection conn = null;
 		PreparedStatement stmt = null;
 		ResultSet rs = null;
-		
-		try{
+
+		try {
 			conn = ConnectionDAO.getInstance().getConnection();
 			stmt = conn.prepareStatement("SELECT * FROM country WHERE idcountry=?");
-		
+
 			stmt.setInt(1, id);
-			
+
 			rs = stmt.executeQuery();
-			
-			if(rs.next()){
+
+			if (rs.next()) {
 				return this.loadObject(rs);
-			}else{
+			} else {
 				return null;
 			}
-		}finally{
-			if((rs != null) && !rs.isClosed())
+		} finally {
+			if ((rs != null) && !rs.isClosed())
 				rs.close();
-			if((stmt != null) && !stmt.isClosed())
+			if ((stmt != null) && !stmt.isClosed())
 				stmt.close();
-			if((conn != null) && !conn.isClosed())
+			if ((conn != null) && !conn.isClosed())
 				conn.close();
 		}
 	}
-	
-	public int save(int idUser, Country country) throws SQLException{
+
+	public int save(int idUser, Country country) throws SQLException {
 		boolean insert = (country.getIdCountry() == 0);
 		Connection conn = null;
 		PreparedStatement stmt = null;
 		ResultSet rs = null;
-		
-		try{
+
+		try {
 			conn = ConnectionDAO.getInstance().getConnection();
-			
-			if(insert){
+
+			if (insert) {
 				stmt = conn.prepareStatement("INSERT INTO country(name) VALUES(?)", Statement.RETURN_GENERATED_KEYS);
-			}else{
+			} else {
 				stmt = conn.prepareStatement("UPDATE country SET name=? WHERE idcountry=?");
 			}
-			
+
 			stmt.setString(1, country.getName());
-			
-			if(!insert){
+
+			if (!insert) {
 				stmt.setInt(2, country.getIdCountry());
 			}
-			
+
 			stmt.execute();
-			
-			if(insert){
+
+			if (insert) {
 				rs = stmt.getGeneratedKeys();
-				
-				if(rs.next()){
+
+				if (rs.next()) {
 					country.setIdCountry(rs.getInt(1));
 				}
 
@@ -103,24 +103,24 @@ public class CountryDAO implements IDaoFactory<Country> {
 			} else {
 				new UpdateEvent(conn).registerUpdate(idUser, country);
 			}
-			
+
 			return country.getIdCountry();
-		}finally{
-			if((rs != null) && !rs.isClosed())
+		} finally {
+			if ((rs != null) && !rs.isClosed())
 				rs.close();
-			if((stmt != null) && !stmt.isClosed())
+			if ((stmt != null) && !stmt.isClosed())
 				stmt.close();
-			if((conn != null) && !conn.isClosed())
+			if ((conn != null) && !conn.isClosed())
 				conn.close();
 		}
 	}
-	
-	private Country loadObject(ResultSet rs) throws SQLException{
+
+	private Country loadObject(ResultSet rs) throws SQLException {
 		Country country = new Country();
-		
+
 		country.setIdCountry(rs.getInt("idcountry"));
 		country.setName(rs.getString("name"));
-		
+
 		return country;
 	}
 
--- src/main/java/br/edu/utfpr/dv/siacoes/dao/DaoFactory.java
+++ src/main/java/br/edu/utfpr/dv/siacoes/dao/DaoFactory.java
@@ -1,15 +1,21 @@
 package br.edu.utfpr.dv.siacoes.dao;
 
 public class DaoFactory {
-    
-    public static IDaoFactory of (String type) {
-        switch (type) {
-            case "Country": return new CountryDAO();
-            case "State": return new StateDAO();
-            case "City": return new CityDAO();
+
+    public static IDaoFactory getDAO(String type) {
+        if (type == null || type.isEmpty()) {
+            return null;
         }
-        
+
+        switch (type) {
+        case "Country":
+            return new CountryDAO();
+        case "State":
+            return new StateDAO();
+        case "City":
+            return new CityDAO();
+        }
+
         return null;
     }
-    
 }
